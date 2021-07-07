1ef3c03f7ae794f8f7dd5115c8f3512c9ef05e76
Mirna <mirnaparra14@gmail.com>
2021-07-07 18:56:14 -0400

    introduciendo efecto smooth scroll

--- assets/js/jqueryviaje.js
+++ assets/js/jqueryviaje.js
@@ -1,11 +1,6 @@
 $(function () { 
 
-    $(".section1").on("click", function (e) {
-        e.preventDefault();
-        const href = $(this).attr("href");
-        $("html, body").animate({ scrollTop: $(href).offset().top }, 1000);
-      });
+    
 
 })
 
-    
